f281591e31dcaf863536a38e8ddf6948a215a1a1
Natalie Holton <natalie.k.holton@gmail.com>
2015-12-09 20:37:02 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nholtonapp::Application.config.secret_key_base = 'da1ddfa8cc3f1146343d17aca0bef914ce38b1f13d40e018b6bbcca30c2dfdd54327dc822e6fe9ab9f7d2e5ec846daecb075c2898460420653057367b91837e6'
